e54d8d7ecffe99c3d64c557734b718580852bab5
Arthur Henrique Costa <37603397+ahtlc@users.noreply.github.com>
2018-05-15 12:22:59 -0300

    Add files via upload

--- TH_05.c
+++ TH_05.c
@@ -6,6 +6,8 @@
 pthread_mutex_t mutex[2];
 int nPorTaxi[2] = {0, 0}, j = -1;
 bool direcao = 0; //0 = esquerda; 1 = direita
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t full = PTHREAD_COND_INITIALIZER;
 
 void *Filas(void *id)
 {
@@ -16,10 +18,18 @@ void *Filas(void *id)
 		if(direcao == 0){
 			if(nPorTaxi[0] < 4){
 				nPorTaxi[0]++;
-				sleep(1);
+				usleep(200000);
+			}else{
+				//mandar sinal que esta cheio
+				//esperar os 500 ms
+				// voltar o numero de pessoas no taxi para 0
+			}
+		}else{
+			if(nPorTaxi[1] < 4){
+				nPorTaxi[1]++;
+				usleep(200000);
 			}
 		}
-		if(j == 2)
 	}
 	pthread_mutex_unlock(&mutex);
 }
